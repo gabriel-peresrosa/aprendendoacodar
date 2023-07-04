aace329ac6f8a53a37ed13e57fa56b26104e18bf
gabriel-peresrosa <gabriel_peresrosa@hotmail.com>
2023-07-04 17:30:50 -0300

    novos commits

---
+++ Adatech-Santander/conversaodetipos.py
@@ -0,0 +1,17 @@
+#conversão de tipos
+
+idade = 18
+
+if idade >=18:
+    print('Você é maior de idade.')
+else:
+    print('Você é menor de idade.')
+
+    media = float(input ('informe a média do estudante'))
+                        
+    if media >= 7:
+        print('Você foi aprovado')
+    elif media >=5:
+        print('Recuperação')
+    else:
+        print('Você foi reprovado')    
---
+++ Adatech-Santander/primeiro.py
@@ -0,0 +1,10 @@
+print('hello world')
+print('seja bem vindo')
+
+input('Qual é')
+
+idade = 26 #criando uma variável
+nome = ('nome meu')
+
+print(idade)
+print(nome)
